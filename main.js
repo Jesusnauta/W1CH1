482fcac7cea8c2303542fb23520768a75c97c906
Jesus <jesus.cassano.2006@gmail.com>
2023-01-16 19:17:55 +0100

    add file

---
+++ main.js
@@ -0,0 +1,25 @@
+const insert = (num) => {
+    let number = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = number + num   
+}
+
+
+const clean = () =>
+{
+document.getElementById('result').innerHTML = "";
+}
+
+const Delete = () =>
+{
+let result = document.getElementById('result').innerHTML;
+document.getElementById('result').innerHTML = result.substring(0, result.length -1);
+}
+
+const calculate = () =>
+{
+let result = document.getElementById('result').innerHTML;
+if(result)
+{
+document.getElementById('result').innerHTML = eval(result)
+}
+}
